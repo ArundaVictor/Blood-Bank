2ebc25dd1664607411846f5edb13313e9b459699
Dun-Njuguna <duncankariuki60@gmail.com>
2019-02-20 10:10:55 +0300

    worked on the interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,5 +10,11 @@ public class App {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";     
 
+        get("/main", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/main.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
     }
 }
