bf08e45ae540922aaf363c756b0d5ad50ca9fa76
Arunda Victor <victoroyugi31@gmail.com>
2019-02-20 10:39:23 +0300

    add login page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,9 +6,24 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+  
+        setPort(port);
 
         staticFileLocation("/public");
-        String layout = "templates/layout.vtl";     
+        String layout = "templates/layout.vtl";
+             
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 
         get("/main", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
