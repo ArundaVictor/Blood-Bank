f9971e5b1d827823bfbe869e04832d3ff76a4b29
Dun-Njuguna <duncankariuki60@gmail.com>
2019-02-22 09:52:44 +0300

    modified homepage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -83,7 +83,11 @@ public class App {
             return new ModelAndView(model, layout);
           }, new VelocityTemplateEngine());
 
-
+          get("/bookSuccess", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/bookSuccess.vtl");
+            return new ModelAndView(model, layout);
+          }, new VelocityTemplateEngine());
         
 
     }
